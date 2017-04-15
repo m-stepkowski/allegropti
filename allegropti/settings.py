10abb3cabbacf96bb97625184e35bd7713e197b2
Maciej Stępkowski <maciek@MacBook-Air-Maciej.local>
2017-04-15 13:08:46 +0200

    Migration process improvement

--- allegropti/settings.py
+++ allegropti/settings.py
@@ -73,18 +73,7 @@ WSGI_APPLICATION = 'allegropti.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
-    }
-else:
+
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
