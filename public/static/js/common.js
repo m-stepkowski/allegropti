bc315d6c134d223bf93cdbd4a1b35c264b7836aa
Maciej Stępkowski <maciek@MacBook-Air-Maciej.local>
2017-04-25 23:05:37 +0200

    Improvement of frontend and js scripts

--- public/static/js/common.js
+++ public/static/js/common.js
@@ -1,6 +1,7 @@
 
-	$('#but_item_search').click(function(){
-	this.form.submit();
-	this.disabled=true;
-	this.innerHTML='<i class="fa fa-spinner fa-spin"></i>';
-	});
+	function form_post(elem){
+		par_post = elem.form;
+		elem.disabled=true;
+		elem.parentElement.innerHTML='<i class="fa fa-spinner fa-spin"></i>';
+		par_post.submit();
+	}
